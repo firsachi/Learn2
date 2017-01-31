ebe4fbd023b713f7ab44f674cd517f3f0b693697
firsachi <firsachi@my-book.kievreclama.local>
2017-01-31 22:52:05 +0200

    add message

--- learn2/src/task2/View.java
+++ learn2/src/task2/View.java
@@ -3,6 +3,7 @@ package task2;
 public class View {
 	
 	public final String message = "\n Input \n \n balance = %d \n withdrawa = %d \n \n Output \n %s";
+	public final String messageArray = "\n Input \n \n owner name = %s \n withdrawa = %d \n \n Output \n %s";
 	
 	public void message(String message){
 		System.out.println(message);
@@ -11,5 +12,8 @@ public class View {
 	public void message(double balance, double withdrawa, String value){
 		message(String.format(message, (int) balance, (int) withdrawa, value));
 	}
-
+	
+	public void message(String ownerName, double withdrawa, String value){
+		message(String.format(messageArray, ownerName, (int) withdrawa, value));
+	}
 }
